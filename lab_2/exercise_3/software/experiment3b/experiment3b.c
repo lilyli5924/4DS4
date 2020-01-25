c16ab7113ba941eced37a4224f5e8a93af948827
lilyli5924 <li851425352@gmail.com>
2020-01-24 20:12:06 -0500

    Fixed indentation.

--- lab_2/exercise_3/software/experiment3b/experiment3b.c
+++ lab_2/exercise_3/software/experiment3b/experiment3b.c
@@ -93,25 +93,25 @@
 #include "altera_up_avalon_character_lcd.h"
 
 alt_u16 disp_seven_seg(alt_u8 val) {
-    switch (val) {
-        case  0 : return 0x40;
-        case  1 : return 0x79;
-        case  2 : return 0x24;
-        case  3 : return 0x30;
-        case  4 : return 0x19;
-        case  5 : return 0x12;
-        case  6 : return 0x02;
-        case  7 : return 0x78;
-        case  8 : return 0x00;
-        case  9 : return 0x18;
-        case 10 : return 0x08;
-        case 11 : return 0x03;
-        case 12 : return 0x46;
-        case 13 : return 0x21;
-        case 14 : return 0x06;
-        case 15 : return 0x0e;
-        default : return 0x7f;
-    }
+	switch (val) {
+    	case  0 : return 0x40;
+    	case  1 : return 0x79;
+    	case  2 : return 0x24;
+    	case  3 : return 0x30;
+    	case  4 : return 0x19;
+    	case  5 : return 0x12;
+    	case  6 : return 0x02;
+    	case  7 : return 0x78;
+    	case  8 : return 0x00;
+    	case  9 : return 0x18;
+    	case 10 : return 0x08;
+    	case 11 : return 0x03;
+    	case 12 : return 0x46;
+    	case 13 : return 0x21;
+    	case 14 : return 0x06;
+    	case 15 : return 0x0e;
+    	default : return 0x7f;
+	}
 }
 
 // Group A interrupt handling function definition
@@ -120,31 +120,32 @@ void SW_GRPA_interrupt(int *amount) {
 	IOWR(LED_GREEN_O_BASE,0,0x1);
 	sw_grp_a = IORD(SWITCH_GRPA_I_BASE, 3);
 
-    if((sw_grp_a & 0x1) == 0x1) {
-        *amount = *amount + 0x1;
-    }
-    if((sw_grp_a & 0x2) == 0x2) {
-        *amount = *amount + 0x2;
-    }
-    if((sw_grp_a & 0x4) == 0x4) {
-        *amount = *amount + 0x5;
-    }
-    if((sw_grp_a & 0x8) == 0x8) {
-        *amount = *amount + 0xa;
-    }
-    alt_printf("%x \n",*amount);
-    //alt_printf("Switches GRPA changed\n");
-    IOWR(SWITCH_GRPA_I_BASE, 3, 0x0);
+	// Pass the value by dereferencing amount pointer
+	if((sw_grp_a & 0x1) == 0x1) {
+    	*amount = *amount + 0x1;
+	}
+	if((sw_grp_a & 0x2) == 0x2) {
+    	*amount = *amount + 0x2;
+	}
+	if((sw_grp_a & 0x4) == 0x4) {
+    	*amount = *amount + 0x5;
+	}
+	if((sw_grp_a & 0x8) == 0x8) {
+    	*amount = *amount + 0xa;
+	}
+	alt_printf("%x \n",*amount);
+	IOWR(SWITCH_GRPA_I_BASE, 3, 0x0);
 }
 
 // Group B interrupt handling function definition
 void SW_GRPB_interrupt(int *amount) {
 	alt_u32 sw_grp_b;
-    sw_grp_b = IORD(SWITCH_GRPB_I_BASE, 3);
-    IOWR(LED_GREEN_O_BASE,0,0x0);
-    if((sw_grp_b & 0x1) == 0x1) {
+	sw_grp_b = IORD(SWITCH_GRPB_I_BASE, 3);
+	IOWR(LED_GREEN_O_BASE,0,0x0);
+	if((sw_grp_b & 0x1) == 0x1) {
     	if (*amount >= 0x29){
-            *amount = *amount - 0x29;
+    		// If amount left is enough to go to Montreal
+        	*amount = *amount - 0x29;
 			IOWR(LED_GREEN_O_BASE,0,0x4);
 			IOWR(SEVEN_SEGMENT_N_O_0_BASE, 0, disp_seven_seg((*amount)%10));
 			IOWR(SEVEN_SEGMENT_N_O_1_BASE, 0, disp_seven_seg((*amount)/10));
@@ -155,15 +156,16 @@ void SW_GRPB_interrupt(int *amount) {
 			alt_printf("%x \n",*amount);
 			*amount = 0x0;
 		}
-    	else {//LED3 FOR 1.5 SECONDS
+    	else {
+    		//LED3 FOR 1.5 SECONDS
 			IOWR(LED_GREEN_O_BASE,0,0x8);
-    		alt_putstr("YOU ARE SO POOR, PLEASE BE RICH THEN COME. \n");
+    		alt_putstr("YOU ARE POOR, PLEASE BE RICH THEN COME. \n");
     		usleep(1500000);
 			IOWR(LED_GREEN_O_BASE,0,0x0);
 			
-        }
+    	}
 	}
-     if((sw_grp_b & 0x2) == 0x2) {
+    if((sw_grp_b & 0x2) == 0x2) {
 		if (*amount >= 0x20){
 			*amount = *amount - 0x20;
 			IOWR(LED_GREEN_O_BASE,0,0x4);
@@ -178,7 +180,7 @@ void SW_GRPB_interrupt(int *amount) {
 		}	
 		else {//LED3 FOR 1.5 SECONDS
 			IOWR(LED_GREEN_O_BASE,0,0x8);
-			alt_putstr("YOU ARE SO POOR, PLEASE BE RICH THEN COME. \n");
+			alt_putstr("YOU ARE POOR, PLEASE BE RICH THEN COME. \n");
 			usleep(1500000);
 			IOWR(LED_GREEN_O_BASE,0,0x0);
 		}	
@@ -197,58 +199,58 @@ void SW_GRPB_interrupt(int *amount) {
 			*amount = 0x0;
 		}
         else {//LED3 FOR 1.5 SECONDS
-            IOWR(LED_GREEN_O_BASE,0,0x8);
-            alt_putstr("YOU ARE SO POOR, PLEASE BE RICH THEN COME. \n");
+        	IOWR(LED_GREEN_O_BASE,0,0x8);
+        	alt_putstr("YOU ARE POOR, PLEASE BE RICH THEN COME. \n");
 			usleep(1500000);
 			IOWR(LED_GREEN_O_BASE,0,0x0);
 		}
 	}
-    if((sw_grp_b & 0x8) == 0x8) {
-        *amount = 0x0;
+	if((sw_grp_b & 0x8) == 0x8) {
+    *amount = 0x0;
 		IOWR(LED_GREEN_O_BASE,0,0x2);
 		usleep(2500000);
 		IOWR(LED_GREEN_O_BASE,0,0x0);
 		alt_printf("%x \n",*amount);
-    }
+	}
 
-    //alt_printf("Switches GRPB changed\n");
-    IOWR(SWITCH_GRPB_I_BASE, 3, 0x0);
+	//alt_printf("Switches GRPB changed\n");
+	IOWR(SWITCH_GRPB_I_BASE, 3, 0x0);
 }
 
 int main()
 { 
-    alt_up_character_lcd_dev *lcd_0;
-    volatile int amount = 0;
-    IOWR(SEVEN_SEGMENT_N_O_0_BASE, 0, 0x7f);
+	alt_up_character_lcd_dev *lcd_0;
+	volatile int amount = 0;
+	IOWR(SEVEN_SEGMENT_N_O_0_BASE, 0, 0x7f);
 	IOWR(SEVEN_SEGMENT_N_O_1_BASE, 0, 0x7f);
-    alt_printf("exercise 3:\n");
+	alt_printf("exercise 3:\n");
     
-    lcd_0 = alt_up_character_lcd_open_dev(CHARACTER_LCD_0_NAME);
+	lcd_0 = alt_up_character_lcd_open_dev(CHARACTER_LCD_0_NAME);
     
-    if (lcd_0 == NULL) alt_printf("Error opening LCD device\n");
-    else alt_printf("LCD device opened.\n");
+	if (lcd_0 == NULL) alt_printf("Error opening LCD device\n");
+	else alt_printf("LCD device opened.\n");
 
-    alt_up_character_lcd_init(lcd_0);
+	alt_up_character_lcd_init(lcd_0);
+	
+	alt_up_character_lcd_string(lcd_0, "Exercise 3");
+	
+	alt_up_character_lcd_set_cursor_pos(lcd_0, 0, 1);
+	
+	alt_up_character_lcd_string(lcd_0, "Welcome");
     
-    alt_up_character_lcd_string(lcd_0, "Exercise 3");
-    
-    alt_up_character_lcd_set_cursor_pos(lcd_0, 0, 1);
-    
-    alt_up_character_lcd_string(lcd_0, "Welcome");
-    
-    // Group A interrupt
-    IOWR(SWITCH_GRPA_I_BASE, 3, 0x0); // edge capture register
-    IOWR(SWITCH_GRPA_I_BASE, 2, 0xF); // IRQ mask
-    alt_irq_register(SWITCH_GRPA_I_IRQ, &amount, (void*)SW_GRPA_interrupt);
+	// Group A interrupt
+	IOWR(SWITCH_GRPA_I_BASE, 3, 0x0); // edge capture register
+	IOWR(SWITCH_GRPA_I_BASE, 2, 0xF); // IRQ mask
+	alt_irq_register(SWITCH_GRPA_I_IRQ, &amount, (void*)SW_GRPA_interrupt);
 
-    // Group B interrupt
-    IOWR(SWITCH_GRPB_I_BASE, 3, 0x0); // edge capture register
-    IOWR(SWITCH_GRPB_I_BASE, 2, 0xF); // IRQ mask
-    alt_irq_register(SWITCH_GRPB_I_IRQ, &amount, (void*)SW_GRPB_interrupt);
+	// Group B interrupt
+	IOWR(SWITCH_GRPB_I_BASE, 3, 0x0); // edge capture register
+	IOWR(SWITCH_GRPB_I_BASE, 2, 0xF); // IRQ mask
+	alt_irq_register(SWITCH_GRPB_I_IRQ, &amount, (void*)SW_GRPB_interrupt);
 
-    /* Event loop never exits. */
-    while (1) {
-    }
+	/* Event loop never exits. */
+	while (1) {
+	}
 
-  return 0;
+	return 0;
 }
